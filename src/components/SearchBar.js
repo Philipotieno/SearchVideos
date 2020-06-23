f8bcf095162a4685293c43d62a93f596fe138ffe
philipotieno <tmitchellb007@gmail.com>
2020-06-24 00:22:41 +0300

    feat: add search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+    state = { term: 'ertyui'};
+
+    onInputChange = () => {
+
+    };
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" value={this.state.term} onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
