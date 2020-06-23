f598eafc9d3935663e20e0d299ec2c474ccc7e48
philipotieno <tmitchellb007@gmail.com>
2020-06-24 00:26:51 +0300

    feat: add event handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-    state = { term: 'ertyui'};
+    state = { term: ''};
 
-    onInputChange = () => {
-
+    onInputChange = event => {
+        this.setState({ term: event.target.value })
     };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+    }
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input type="text" value={this.state.term} onChange={this.onInputChange} />
